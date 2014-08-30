b6c799f58ed020535e997e7c5156cf4735bb7e4b
Jack Connors <Jconnors002@aol.com>
2014-08-30 23:14:03 +0100

    Fixed background audio update

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 		<script src="js/jquery.scrollgress.min.js"></script>
 		<script src="js/skel.min.js"></script>
 		<script src="js/init.js"></script>
-		<script src="js/togglesound.js"</script>
+		<script src="js/togglesound.js"></script>
 		<noscript>
 			<link rel="stylesheet" href="css/skel.css" />
 			<link rel="stylesheet" href="css/style.css" />
