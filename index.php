cdd224d724e9946474e43dffeab1ee2287d3d4c6
Jack Connors <Jconnors002@aol.com>
2014-08-30 17:15:14 +0100

    WP Category update 2

--- index.php
+++ index.php
@@ -89,7 +89,7 @@
 						<?php 
 							require($_SERVER['DOCUMENT_ROOT'] . '/blog/wp-load.php'); 
 							$args = array(
-								'cat' => 3, // Only source posts from a specific category
+								'cat' => 1, // Only source posts from a specific category
 								'posts_per_page' => 2 // Specify how many posts you'd like to display
 							);
 							$latest_posts = new WP_Query( $args ); 	
